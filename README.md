1585f9ec63ad4e2e0b25d8e5fe32c055aea30a44
lzx6666 email = 1132721815@qq.com <1132721815@qq.com>
2019-08-10 17:53:07 +0800

    add flyway migrate support

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 [es](https://elasticsearch.cn/)   
 [github]  
 [Github OAuth](https://developer.github.com/apps/building-github-apps/creating-a-github-app/)
+[Spring](https://docs.spring.io/spring-boot/docs/2.1.6.RELEASE/reference/html/boot-features-sql.html)  
 ## 工具
 [Git](https://git-scm.com/download)  
 [Visual Paradigm](https://www.visual-paradigm.com)  
--- src/main/java/com/ldlzx/community/controller/AuthorizeController.java
+++ src/main/java/com/ldlzx/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.ldlzx.community.controller;
 
 import com.ldlzx.community.dto.AccessTokenDTO;
 import com.ldlzx.community.dto.GithubUser;
+import com.ldlzx.community.mapper.UserMapper;
+import com.ldlzx.community.model.User;
 import com.ldlzx.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,9 +29,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,8 +43,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            //登录成功，写cookie 和 session
+            User user =  new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            //登录失败重新登录
+            return "redirect:/";
+        }
     }
 }
